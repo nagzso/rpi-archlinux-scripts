294633a789c5871d597ffc16ad60f68fb0822be8
Zsolt Nagy <nagzso14@gmail.com>
2017-11-07 00:07:25 +0100

    Upgrade system.

--- sudo.sh
+++ sudo.sh
@@ -3,6 +3,7 @@
 XDEFAULT_EDITOR="${1}"
 XSUDOERS_DIR='/etc/sudoers.d';
 
+pacman -Syu --noconfirm;
 pacman -S --needed --noconfirm sudo;
 
 [ -z "${XDEFAULT_EDITOR}" ] && exit 0;
@@ -13,7 +14,7 @@ pacman -S --needed --noconfirm sudo;
 
 echo "Defaults editor=/usr/bin/${XDEFAULT_EDITOR}, !env_editor" > "${XSUDOERS_DIR}/editor";
 
-chown -c -R root:root "${XSUDOERS_DIR}";
-chmod -c -R 0440 ${XSUDOERS_DIR}/*;
+chown -R root:root "${XSUDOERS_DIR}";
+chmod -R 0440 ${XSUDOERS_DIR}/*;
 
 exit 0;
