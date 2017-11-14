aa947309f52a3e03815e9fc5b1a4957c7aa4ea55
Zsolt Nagy <nagzso14@gmail.com>
2017-11-14 22:13:20 +0100

    Alsa and codec initializer script.

---
+++ media.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function install() {
+  pacman -Sy --needed --noconfirm mesa;
+  pacman -S --needed --noconfirm ffmpeg;
+  pacman -S --needed --noconfirm mpg123;
+  pacman -S --needed --noconfirm lame;
+  pacman -S --needed --noconfirm flac;
+  pacman -S --needed --noconfirm libmpeg2;
+  pacman -S --needed --noconfirm xvidcore;
+  pacman -S --needed --noconfirm x264;
+  pacman -S --needed --noconfirm x265;
+  pacman -S --needed --noconfirm alsa-utils;
+  pacman -S --needed --noconfirm alsa-plugins;
+  pacman -S --needed --noconfirm alsa-firmware;
+}
+
+function configure() {
+  echo "dtparam=audio=on" >> /boot/config.txt;
+}
+
+install;
+configure;
+
+exit 0;
