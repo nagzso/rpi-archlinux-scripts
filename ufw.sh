be1d71a302baee80ca47f21d8e4397b71a7f618d
Zsolt Nagy <nagzso14@gmail.com>
2017-11-14 20:49:54 +0100

    Subnet is in-parameter for ufw.

--- ufw.sh
+++ ufw.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+XLAN="${1}";
+
 pacman -S --needed --noconfirm ufw;
 
 ufw default deny incoming;
 ufw default allow outgoing;
-ufw allow from 192.168.0.0/24;
+ufw allow from "${XLAN}";
 ufw limit SSH;
 
 yes | ufw enable;
